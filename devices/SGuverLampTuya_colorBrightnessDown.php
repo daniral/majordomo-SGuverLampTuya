2e274b77fdb372c16bead84f745748d062695d85
daniral <daniralgal@gmail.com>
2025-10-28 06:52:00 +0200

    яркость цвета up down

--- devices/SGuverLampTuya_colorBrightnessDown.php
+++ devices/SGuverLampTuya_colorBrightnessDown.php
@@ -1,2 +1,9 @@
 <?php
 
+/*
+Уменьшить яркость на (array("value"=>1--100)). Без  параметров на 10.
+*/
+$colorBrightness = $this->getProperty('colorBrightness');
+$inc = isset($params['value']) && is_numeric($params['value']) ? max(1, min(100, $params['value'])):10;
+$colorBrightness = max(0, $colorBrightness - $inc);
+$this->setProperty('colorBrightness', $colorBrightness);
--- devices/SGuverLampTuya_colorBrightnessUp.php
+++ devices/SGuverLampTuya_colorBrightnessUp.php
@@ -1,2 +1,9 @@
 <?php
 
+/*
+Увеличить яркость.(array('value'=>1--100)). Без  параметров +10.
+*/
+$colorBrightness = $this->getProperty('colorBrightness');
+$inc = isset($params['value']) && is_numeric($params['value']) ? max(1, min(100, $params['value'])):10;
+$colorBrightness = min(100, $colorBrightness + $inc);
+$this->setProperty('colorBrightness', $colorBrightness);
